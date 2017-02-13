58aff17a4da9d378e2b262806cfcb229030265e4
Borja Egea Madrid <boegma@hotmail.com>
2017-02-13 01:42:07 +0100

    Ejercicio de las Tablas de Multiplicar.

--- tablasmult.py
+++ tablasmult.py
@@ -1,10 +1,10 @@
-#!/urs/bin/python3
+#!/usr/bin/python3
 
 # Tablas de multiplicar del 1 al 10
 
 for indice1 in range(1,11):
-	print "Tabla del "+ str(indice1)
-	print "-----------"
+	print ("Tabla del "+ str(indice1))
+	print ("-----------")
 
 	for indice2 in range(1,11):
 		print(str(indice1)+" por "+ str(indice2)+" es "+str(indice1*indice2))
